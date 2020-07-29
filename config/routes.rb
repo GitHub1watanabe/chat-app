847ee63fae5b3282e665ec63c957f631b6b81464
GitHub1watanabe <68591967+GitHub1watanabe@users.noreply.github.com>
2020-07-29 17:12:19 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "rooms#index"
   get 'messages/index'
-  # root "messages#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
